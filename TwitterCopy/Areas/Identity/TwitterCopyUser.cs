3be1230560760a777b20af91bd0e114821788ecd
RouSage <therousage@gmail.com>
2018-06-23 18:17:33 +0300

    Tweet table(entity) added

--- TwitterCopy/Areas/Identity/TwitterCopyUser.cs
+++ TwitterCopy/Areas/Identity/TwitterCopyUser.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using TwitterCopy.Models;
 
 namespace TwitterCopy.Areas.Identity
 {
     // Add profile data for application users by adding properties to the TwitterCopyUser class
     public class TwitterCopyUser : IdentityUser<Guid>
     {
+        [PersonalData]
+        public ICollection<Tweet> Tweets { get; set; }
     }
 }
---
+++ TwitterCopy/Models/Tweet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TwitterCopy.Areas.Identity;
+
+namespace TwitterCopy.Models
+{
+    public class Tweet
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(280)]
+        public string Text { get; set; }
+
+        public Guid UserId { get; set; }
+
+        [Required]
+        [ForeignKey("UserId")]
+        public TwitterCopyUser User { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime PostedOn { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Retweets { get; set; }
+    }
+}
