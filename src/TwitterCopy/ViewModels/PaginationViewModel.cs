e5f2f6faad84632c98cf6a924f306c5f854f5927
RouSage <therousage@gmail.com>
2018-10-07 14:35:17 +0300

    _PaginationPartial partial view and PaginationViewModel created

---
+++ src/TwitterCopy/ViewModels/PaginationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TwitterCopy.Models
+{
+    public class PaginationViewModel
+    {
+        public int CurrentPage { get; set; } = 1;
+
+        public int Count { get; set; }
+
+        public int PageSize { get; set; } = 5;
+
+        public int TotalPages => (int)Math.Ceiling(decimal.Divide(Count, PageSize));
+
+        public bool ShowPrevious => CurrentPage > 1;
+
+        public bool ShowNext => CurrentPage < TotalPages;
+
+        public bool ShowFirst => CurrentPage != 1;
+
+        public bool ShowLast => CurrentPage != TotalPages;
+    }
+}
